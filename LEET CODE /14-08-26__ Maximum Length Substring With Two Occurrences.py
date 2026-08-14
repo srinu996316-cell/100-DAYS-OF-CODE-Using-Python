5f6e6bda5926b4a1a50dc204850d9e219b965806
srinu996316-cell <srinu996316@gmail.com>
2026-08-14 11:10:47 +0530

    Implement maximum length substring with two occurrences

---
+++ LEET CODE /14-08-26__ Maximum Length Substring With Two Occurrences.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maximumLengthSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        freq = {}
+        left = 0
+        ans = 0
+
+        for right in range(len(s)):
+            freq[s[right]] = freq.get(s[right], 0) + 1
+
+            while freq[s[right]] > 2:
+                freq[s[left]] -= 1
+                left += 1
+
+            ans = max(ans, right - left + 1)
+
+        return ans
