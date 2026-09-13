d8ed8714957abfa9b63691be89493a32b795a5d5
srinu996316-cell <srinu996316@gmail.com>
2026-09-13 20:41:55 +0530

    Add largestOverlap method to calculate image overlap
    Implement a method to calculate the largest overlap between two images represented as 2D arrays.

---
+++ LEET CODE /13-09-26__Image Overlap.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def largestOverlap(self, img1, img2):
+        """
+        :type img1: List[List[int]]
+        :type img2: List[List[int]]
+        :rtype: int
+        """
+
+        n = len(img1)
+        ans = 0
+
+        for dr in range(-n + 1, n):
+            for dc in range(-n + 1, n):
+                overlap = 0
+
+                for r in range(n):
+                    for c in range(n):
+                        nr = r + dr
+                        nc = c + dc
+
+                        if 0 <= nr < n and 0 <= nc < n:
+                            if img1[r][c] == 1 and img2[nr][nc] == 1:
+                                overlap += 1
+
+                ans = max(ans, overlap)
+
+        return ans
