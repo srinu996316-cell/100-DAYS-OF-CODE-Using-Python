433eb9ea33c2d6abd105a4551950d100bc6ed116
srinu996316-cell <srinu996316@gmail.com>
2026-09-24 11:41:26 +0530

    Smallest Index With Digit Sum Equal to Index

---
+++ LEET CODE /24-09-26__Smallest Index With Digit Sum Equal to Index.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def smallestIndex(self, nums):
+        for i, num in enumerate(nums):
+            if sum(map(int, str(num))) == i:
+                return i
+        return -1
